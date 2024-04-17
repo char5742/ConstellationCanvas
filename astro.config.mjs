54e2ce120ae34ca685912f3002558c65634a11cc
char5742 <fujino@stellarcode.jp>
2024-04-17 20:47:32 +0900

    fix: sitemap url

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ww.stellarcode.dev",
+  site: "https://www.stellarcode.dev",
   integrations: [
     mdx(),
     sitemap(),
